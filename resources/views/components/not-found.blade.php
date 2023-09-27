9cb1b45b9cafaf2c4d44843edea917ddf0304aba
algrvvv <alexandrgr25@gmail.com>
2023-09-27 16:07:42 +0300

    custom components

---
+++ resources/views/components/not-found.blade.php
@@ -0,0 +1,4 @@
+<div class="flex flex-col justify-center items-center mt-10">
+    <p class="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
+    <p class="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
+</div>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -40,10 +40,7 @@
                 <x-comment :comment="$comment"></x-comment>
             @endforeach
         @else
-            <div class="flex flex-col justify-center items-center mt-10">
-                <p class="text-2xl font-semibold md:text-3xl">Oops, there's not a single comment here!</p>
-                <p class="mt-4 mb-8 dark:text-gray-400">You can be the first!</p>
-            </div>
+            <x-no-comment></x-no-comment>
         @endif
 
     </div>
